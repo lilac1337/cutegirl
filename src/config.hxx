0bcc1b60547467bbc3e6f3ec886eaf5a27384a93
lilac1337 <pinkknight1@protonmail.com>
2024-03-17 22:42:53 -0400

    readme update

--- src/config.hxx
+++ src/config.hxx
@@ -6,14 +6,7 @@
 #include <variant>
 
 namespace config {
-//	inline char prefix = ';';
-
     inline std::unordered_map<std::string, std::string> settings;
-
-/*	inline std::string server = "cutehops.net";
-    inline std::uint16_t port = 64738;
-    inline std::string username = "cutegirl";
-    inline std::string password = "superSecurePassword";*/
-
+    
     void loadConfig();
 };
